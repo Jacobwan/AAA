1f77670a2950a2a8c13cc6802477d1ddae4581e2
lao_wan <lao_wan@sina.cn>
2016-12-16 19:07:14 +0800

    123

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from blog import views
+
+urlpatterns = [
+    url(r'^blog/$', views.IndexView.as_view(), name='home'),
+]
+"""
+    url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
+    url(r'^category/(?P<cate_id>\d+)$', views.CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<tag_id>\d+)$', views.TagView.as_view(), name='tag'),
+    url(r'^archive/(?P<year>\d+)/(?P<month>\d+)$', views.ArchiveView.as_view(), name='archive'),
+    url(r'^article/(?P<article_id>\d+)/comment/$', views.CommentPostView.as_view(), name='comment'),
+"""
+"""
+   url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
+    url(r'^category/(?P<cate_id>\d+)$', views.CategoryView.as_view(), name='category'),
+"""
