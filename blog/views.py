56ef3fbe9a32fe81d11101b31c4e69eab0f064c4
lao_wan <lao_wan@sina.cn>
2017-01-12 21:08:49 +0800

    20170112考虑增加分类

--- blog/views.py
+++ blog/views.py
@@ -53,3 +53,27 @@ class ArticleDetailView(DetailView):
         obj = super(ArticleDetailView, self).get_object()
         obj.body = markdown2.markdown(obj.body)
         return obj
+
+
+class CategoryView(ListView):
+# 继承自ListView,用于展示一个列表
+    template_name = "blog/index.html"
+    # 指定需要渲染的模板
+    context_object_name = "article_list"
+    # 指定模板中需要使用的上下文对象的名字
+
+    def get_queryset(self):
+         """
+         过滤数据，获取所有已发布文章，并且将内容转成markdown形式
+        """
+        article_list = Article.objects.filter(category=self.kwargs['cate_id'], status='p')
+        # 获取数据库中的所有已发布的文章，即filter(过滤)状态为'p'(已发布)的文章
+        for article in article_list:
+            article.body = markdown2.markdown(article.body, extras=['fenced-code-blocks'], )
+            # 将markdown标记的文本转为html文本
+        return article_list
+
+    def get_context_data(self, **kwargs):
+        # 增加额外的数据，这里返回一个文章分类，以字典的形式
+        kwargs['category_list'] = Category.objects.all().order_by('name')
+        return super(CategoryView, self).get_context_data(**kwargs)
